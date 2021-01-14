c9d606fbf035440c5dad580354026095d99216c7
Pavel Romanov <Pavelromanov2016@gmail.com>
2021-01-14 23:08:58 +0300

    Added a new feature

---
+++ venv/task 1.py
@@ -0,0 +1,10 @@
+a = 2
+b = 4
+c = 6
+d = 8
+print("Переменные a и b -", a, b, c, d)
+string1 = input("Введите первую строку: ")
+number1 = int(input("Введите первое число: "))
+string2 = input("Введите вторую строку: ")
+number2 = int(input("Введите второе число: "))
+print("Введеные значения:", string1, number1, string2, number2)
